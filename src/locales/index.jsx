9ac74709de87ba98af433bf3f176ef477bd26a47
klthemathnerd8 <91514631+klthemathnerd8@users.noreply.github.com>
2023-09-28 18:52:49 -0700

    Update index.jsx

--- src/locales/index.jsx
+++ src/locales/index.jsx
@@ -1,9 +1,5 @@
 import en from "./en/index.jsx"
-import es from "./es/index.jsx"
-import fr from "./fr/index.jsx"
-import zh from "./zh/index.jsx"
-import de from "./de/index.jsx"
 
-var languages = {en, es, fr, zh, de}
+var languages = {en}
 
 export default languages;
